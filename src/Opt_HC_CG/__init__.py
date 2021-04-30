b967c874a361f8b41cb7c48f93f70357f931cf46
Carlosrlpzi <carlosrlpzi@gmail.com>
2021-04-29 21:57:00 -0500

    new file conjugate gradiant package #4

--- src/Opt_HC_CG/__init__.py
+++ src/Opt_HC_CG/__init__.py
@@ -1,2 +1,5 @@
-from hill import best_solution
-from hill import distance_matrix
+from Opt_HC_CG.hill import best_solution
+from Opt_HC_CG.hill import distance_matrix
+from Opt_HC_CG.grad_conj import cgm
+from Opt_HC_CG.grad_conj import its_simetric
+from Opt_HC_CG.grad_conj import is_pos_defix
