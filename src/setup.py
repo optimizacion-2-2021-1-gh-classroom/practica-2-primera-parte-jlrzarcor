38cb54a8ec18c8db63baff5fa4f82ad82e7b73e6
Carlosrlpzi <53799984+Carlosrlpzi@users.noreply.github.com>
2021-05-01 15:10:40 -0500

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -13,6 +13,9 @@ setup(
   install_requires=[            # I get to this in a second
           'numpy',
           'pandas',
+          'cvxpy',
+          'scikit-learn',
+           'pytest'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
