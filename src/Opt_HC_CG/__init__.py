647f883040a1704c23ae02d5f8535ab04cbaa2af
Carlosrlpzi <carlosrlpzi@gmail.com>
2021-05-01 13:45:39 -0500

    configurable parameters addded #4

--- src/Opt_HC_CG/__init__.py
+++ src/Opt_HC_CG/__init__.py
@@ -2,4 +2,4 @@ from Opt_HC_CG.hill import best_solution
 from Opt_HC_CG.hill import distance_matrix
 from Opt_HC_CG.grad_conj import cgm
 from Opt_HC_CG.grad_conj import its_simetric
-from Opt_HC_CG.grad_conj import is_pos_defix
+from Opt_HC_CG.grad_conj import is_pos_def
