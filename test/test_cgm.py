18942ec61be75bffde796fa16a95d507b6919901
Miguel López Cruz <miguellcl@hotmail.com>
2021-05-02 03:12:10 -0500

    Se corrige otro error en el nombre del módulo#6

--- test/test_cgm.py
+++ test/test_cgm.py
@@ -4,7 +4,7 @@ import os
 
 os.chdir("../")
 
-import src.Opt_HC_CH.grad_conj as opt 
+import src.Opt_HC_CG.grad_conj as opt 
 
 a = np.array([[3, 2, -1], [2, -1, 1], [-1, 1, -1]])
 b = np.array([1, -2, 0])
