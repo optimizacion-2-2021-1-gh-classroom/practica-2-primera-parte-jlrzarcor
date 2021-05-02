77dd63b80acecce87c19ee082ab17f51f182fd89
Miguel López Cruz <miguellcl@hotmail.com>
2021-05-02 02:44:27 -0500

    Se agrega test para checar el módulo creado #6

---
+++ test/test_cgm.py
@@ -0,0 +1,16 @@
+import pytest
+import numpy as np
+import os
+
+os.chdir("../")
+
+import src.Opt_HC_CG.functions as opt 
+
+a = np.array([[3, 2, -1], [2, -1, 1], [-1, 1, -1]])
+b = np.array([1, -2, 0])
+x = np.linalg.solve(a, b)
+
+
+def test_cgm():
+    x_sol = opt.cgm(a,b,np.array([1,1,1]))
+    assert np.linalg.norm(x-x_sol) < 1e-10
