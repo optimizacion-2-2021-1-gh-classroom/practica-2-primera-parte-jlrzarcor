19b818c6bbffead63417b3791a5a9db7a116688c
Miguel López Cruz <miguellcl@hotmail.com>
2021-05-03 03:15:21 -0500

    Se modifica script para el testing #6

--- test/test_hillclimbing.py
+++ test/test_hillclimbing.py
@@ -1,15 +1,13 @@
-#dummy line
 import pytest
 import numpy as np
 import os
-
+from python_tsp.distances import euclidean_distance_matrix
+from python_tsp.exact import solve_tsp_dynamic_programming
 
 os.chdir("../")
 
 import src.Opt_HC_CG.best_solution as opt 
 
-from python_tsp.distances import great_circle_distance_matrix
-from python_tsp.exact import solve_tsp_dynamic_programming
 
 sources = np.array([
     [40.73024833, -73.79440675],
@@ -17,11 +15,12 @@ sources = np.array([
     [41.26591   , -73.21026228],
     [41.3249908 , -73.507788  ]
 ])
-distance_matrix = great_circle_distance_matrix(sources)
+
+distance_matrix = euclidean_distance_matrix(sources)
 permutation, distance = solve_tsp_dynamic_programming(distance_matrix)
 
 
 def test_distance():
-    dist_opt = opt.best_solution(sources)
+    dist_opt = best_solution(sources)
     assert np.linalg.norm(distance - dist_opt) < 1e-3  
     
